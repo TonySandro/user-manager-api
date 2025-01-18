7b68bf40184a62975b240c19f96c0fc88fc2d4f0
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-18 18:20:42 -0300

    refactor: change account factory to signup

--- src/main/routes/api-routes.ts
+++ src/main/routes/api-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
-import { makeAccountController } from "../factories/account/account-factory";
+import { makeAccountController } from "../factories/signup/signup-factory";
 import { makeLoginController } from "../factories/login/login-factory";
 
 export default (router: Router): void => {
