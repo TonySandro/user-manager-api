5a28b844a1001e074fc7f9decd5fa24749635f2a
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-02-06 12:25:26 -0300

    refactor: adding return in AccessTokenController

--- src/presentation/controller/access-token/access-token-controller.ts
+++ src/presentation/controller/access-token/access-token-controller.ts
@@ -1,6 +1,6 @@
 import { UpdateAccessTokenRepository } from "../../../data/protocols/database";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { serverError } from "../../helpers/http/http-helper";
+import { serverError, success } from "../../helpers/http/http-helper";
 
 export class AccessTokenController implements Controller {
   constructor(
@@ -11,6 +11,7 @@ export class AccessTokenController implements Controller {
     try {
       const { id, accessToken } = httpRequest.body;
       await this.updateAccessToken.updateAccessToken(id, accessToken);
+      return success("Update access token success.");
     } catch (error) {
       return serverError(error);
     }
