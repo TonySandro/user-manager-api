5e4b79781af4cb7a98f601b9a174702aa4ba507b
Tony Sandro <tonysduarte101@gmail.com>
2025-07-09 09:29:41 -0300

    feat: creating the email confirmation route via token

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,11 @@
 import { Express, Router } from "express";
 import { readdirSync } from "node:fs";
+import confirmationRoute from "../routes/email-confirmation";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
+  app.use("/api", confirmationRoute);
 
   readdirSync(`${__dirname}/../routes`).map(async (file) => {
     if (!file.includes(".test.")) {
---
+++ src/main/routes/email-confirmation.ts
@@ -0,0 +1,48 @@
+import { Request, Response, Router } from "express";
+import { AppDataSource } from "../config/typeorm.config";
+import { AccountModel } from "../../domain/models/account";
+
+const confirmationRoute = Router();
+
+confirmationRoute.get(
+  "/confirm-email",
+  async (req: Request, res: Response): Promise<any> => {
+    const { token } = req.query;
+
+    if (!token || typeof token !== "string") {
+      return res.status(400).send("Token inválido");
+    }
+
+    try {
+      const accountRepository = AppDataSource.getRepository(AccountModel);
+
+      const account = await accountRepository.findOne({
+        where: { emailConfirmationToken: token },
+      });
+
+      if (!account) {
+        return res.status(404).send("Conta não encontrada");
+      }
+
+      if (
+        !account.emailConfirmationExpiresAt ||
+        account.emailConfirmationExpiresAt < new Date()
+      ) {
+        return res.status(400).send("Token expirado");
+      }
+
+      account.isConfirmed = true;
+      account.emailConfirmationToken = null;
+      account.emailConfirmationExpiresAt = null;
+
+      await accountRepository.save(account);
+
+      return res.send("E-mail confirmado com sucesso!");
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Erro ao confirmar o e-mail");
+    }
+  }
+);
+
+export default confirmationRoute;
